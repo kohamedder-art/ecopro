bc4b0d50a286d63810590841c98b9163e430ba1e
Your Name <you@example.com>
2026-07-16 13:45:55 +0100

    fix: add nonce to Facebook Pixel inline script tags so CSP allows execution

--- server/index.ts
+++ server/index.ts
@@ -2132,8 +2132,9 @@ ${urls}
               }
 
               for (const pid of fbPixelIds) {
+                const nonceAttr = nonce ? ` nonce="${nonce}"` : '';
                 const pixelCode = `
-<script>
+<script${nonceAttr}>
 !function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
