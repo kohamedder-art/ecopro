a8100cb6ea4e4b168180aa4db59fa02635088ac4
Your Name <you@example.com>
2026-07-21 19:49:32 +0100

    remove redundant inline fbq PageView track from server HTML

--- server/index.ts
+++ server/index.ts
@@ -2153,7 +2153,6 @@ t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '${pid.replace(/'/g, "\\'")}');
-fbq('track', 'PageView');
 </script>
 <noscript><img height="1" width="1" style="display:none"
 src="https://www.facebook.com/tr?id=${encodeURIComponent(pid)}&ev=PageView&noscript=1"/>
